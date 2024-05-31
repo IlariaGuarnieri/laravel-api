098fb0bef31775675883b1aa1dbc7349b51a9706
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-31 17:16:40 +0200

    [add] milestone 2

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::all();
+
+        return response()->json($projects);
+
+        //non uso compact perche devo passare solo un dato, ovvero projects, se devo passare piu di un dato uso compact
+    }
 }
