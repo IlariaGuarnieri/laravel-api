b39f8667e860dd8d5646bec165fdd71aec884936
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-06-04 19:13:27 +0200

    [add] problemi con la stama in pagina del progetto parallello vite boolfolio

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,7 +17,21 @@ class ProjectController extends Controller
         //non uso compact perche devo passare solo un dato, ovvero projects, se devo passare piu di un dato uso compact
     }
 
-    public function getProjectBySlug($slug){
-        dd($slug);
+    public function getProjectBySlug($slug)
+    {
+        $project = Project::where('slug', $slug)->with('technologies')->first();
+        if ($project) {
+            $success = true;
+            if ($project->image) {
+                $project->image = asset('storage/' . $project->image);
+            } else {
+                $project->image = asset('storage/uploads/no-image.jpeg');
+                $project->original_image = 'no-image';
+            }
+        } else {
+            $success = false;
+        }
+        return response()->json(compact('project', 'success'));
+        // dd($slug);
     }
 }
