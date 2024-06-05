dbf051284e5dd9b918b6c4b34304db19cd1f070b
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-06-05 17:23:23 +0200

    [add] fine email

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewContact as MailNewContact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
 use App\Models\Lead;
+use App\Mail\NewContact;
 
 class LeadController extends Controller
 {
@@ -41,10 +44,8 @@ class LeadController extends Controller
         $new_lead ->fill($data);
         $new_lead ->save();
 
-
         //invio email
-
-
+        Mail::to(env('MAIL_FROM_ADDRESS'))->send(new NewContact($new_lead));
         //restituisco json con avvenuto invio
 
         $success = true;
---
+++ resources/views/email/new_contact.blade.php
@@ -0,0 +1,7 @@
+<p>
+    Hai ricevuto un messaggio da: <br>
+    Nome: {{$lead->name}} <br>
+    Email: {{$lead->email}} <br>
+    Messaggio: {{$lead->message}}
+
+</p>
